43ad75cc95a2b53f59bbee9c0def25142d724650
marwan <marwanabdalla1999@gmail..com>
2023-09-21 02:15:45 +0300

    ..

--- app/src/test/java/com/example/task/imagesScreen/viewModelTest/ImageViewModelTest.kt
+++ app/src/test/java/com/example/task/imagesScreen/viewModelTest/ImageViewModelTest.kt
@@ -1,23 +1,18 @@
 package com.example.task.imagesScreen.viewModelTest
 
 
-import androidx.lifecycle.Observer
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.task.dataSource.interfaces.IDataSource
 import com.example.task.imagesScreen.viewModels.ImagesViewModel
 import com.example.task.virtualData.VirtualData
 import com.example.task.virtualRepository.VirtualImagesRepository
-import junit.framework.TestCase.assertNotNull
-import junit.framework.TestCase.assertTrue
+
 import kotlinx.coroutines.test.runTest
-import org.junit.After
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.Mock
-import org.mockito.Mockito.verify
-import org.mockito.MockitoAnnotations
+
 
 
 @RunWith(AndroidJUnit4::class)
